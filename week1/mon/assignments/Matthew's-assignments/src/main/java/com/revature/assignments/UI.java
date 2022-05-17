95fb28862e9c9c96b6258e3c11c6452e2e9cc6b8
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-05-17 10:22:59 -0700

    day 1 challenge

---
+++ week1/mon/assignments/Matthew's-assignments/src/main/java/com/revature/assignments/UI.java
@@ -0,0 +1,12 @@
+package com.revature.assignments;
+
+
+public class UI {
+    public static void main (String args[]){
+        Solution temp = new Solution();
+        System.out.println(temp.sumOfTwoNum(1,3));
+        System.out.println(temp.evenOdd(1));
+        System.out.println(temp.abbreviation("localization"));
+    }
+
+}
