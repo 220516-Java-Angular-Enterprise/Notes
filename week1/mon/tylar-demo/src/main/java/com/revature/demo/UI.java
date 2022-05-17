3d9ea06dd83afd4a074d88f36c729869f432df66
trknox <love.lala2003@gmail.com>
2022-05-17 17:27:55 -0400

    update dog code

--- week1/mon/tylar-demo/src/main/java/com/revature/demo/UI.java
+++ week1/mon/tylar-demo/src/main/java/com/revature/demo/UI.java
@@ -2,9 +2,9 @@ package com.revature.demo;
 
 public class UI {
     public static void main(String[] args) {
-        Dog myDog = new Dog("O", "g", 4, 'l', 'f');
+        Dog myDog = new Dog();
 
         myDog.setAge(4);
-        //System.out.print(myDog.toString());
+        System.out.print(myDog);
     }
 }
