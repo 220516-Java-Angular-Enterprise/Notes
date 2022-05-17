564fdc762f939e5ad0b8d2acdea9c2790f80d6b7
Hal Coronado <hallie_coronado1@baylor.edu>
2022-05-17 09:42:12 -0500

    hmmm

---
+++ week1/mon/assignments/src/main/java/com/revature/assignments/Solutions.java
@@ -0,0 +1,42 @@
+package com.revature.assignments;
+
+public class Solutions {
+    //add two integers//
+    public String sumofTwoNum(int a, int b) {
+        int sum = a + b;
+        return "the sum of " + a + ", " + b + " is " + sum;
+    }
+
+    //return yes if even//
+    //no if odd//
+    public String evenOdd(int a) {
+        if (a % 2 == 0) {
+            return (a + " is an even int");
+        } else {
+            return (a + " is an odd int");
+        }
+    }
+
+    //if string length is strictly 10+ characters//
+    //string should be replaced with special abbreviation//
+    //else, return the word//
+    public String abbreviate(String c) {
+        int n = c.length();
+        if (n >= 10) {
+            int word_len = (c.length() - 2);
+            String p = String.valueOf(word_len);
+            char first = c.charAt(0);
+            char last = c.charAt(n - 1);
+
+            String first1 = String.valueOf(first);
+            String last1 = String.valueOf(last);
+
+            return first1+p+last1;
+        }else {
+            return c;
+        }
+
+    }
+
+
+}
---
+++ week1/mon/assignments/src/main/java/com/revature/assignments/UI.java
@@ -0,0 +1,12 @@
+package com.revature.assignments;
+
+public class UI {
+    public static void main(String[] args) {
+        Solutions solved = new Solutions();
+
+        System.out.println(solved.sumofTwoNum(4,6));
+        System.out.println(solved.evenOdd(5));
+        System.out.println(solved.abbreviate("internationalization"));
+    }
+
+}
