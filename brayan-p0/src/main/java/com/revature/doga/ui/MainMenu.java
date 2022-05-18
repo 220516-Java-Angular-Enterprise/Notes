d514b097dd25f811bc0073ab415ed3274a70cd83
Brayan <baviles599@gmail.com>
2022-05-18 12:35:49 -0500

    Test

--- brayan-p0/src/main/java/com/revature/doga/ui/MainMenu.java
+++ brayan-p0/src/main/java/com/revature/doga/ui/MainMenu.java
@@ -3,7 +3,8 @@ import com.revature.doga.models.User;
 public class MainMenu implements IMenu{
     private final User user;
 
-    public MainMenu() {
+    public MainMenu(User user) {
+        this.user = user;
     }
 
     @Override
