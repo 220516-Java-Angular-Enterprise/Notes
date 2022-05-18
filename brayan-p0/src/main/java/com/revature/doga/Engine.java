e4fd1754f5c3c0c9c7d7f96372e5b4e7297f536a
Brayan <baviles599@gmail.com>
2022-05-18 12:54:55 -0500

    Deleted Files

--- brayan-p0/src/main/java/com/revature/doga/Engine.java
+++
@@ -1,9 +0,0 @@
-package com.revature.doga;
-
-import com.revature.doga.ui.StartMenu;
-
-public class Engine {
-    public static void main(String[] args) {
-        new StartMenu().start();
-    }
-}
--- brayan-p0/src/main/java/com/revature/doga/ui/IMenu.java
+++
@@ -1,5 +0,0 @@
-package com.revature.doga.ui;
-
-public interface IMenu {
-    void start();
-}
