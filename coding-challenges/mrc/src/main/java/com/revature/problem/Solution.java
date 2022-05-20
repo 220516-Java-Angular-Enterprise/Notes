6e62d5d776730252786c19ded211c29e79ff6917
Joseph Choi <joseph375@revature.net>
2022-05-19 21:14:58 -0400

    Completed coding challenge.

--- coding-challenges/mrc/src/main/java/com/revature/problem/Solution.java
+++ coding-challenges/mrc/src/main/java/com/revature/problem/Solution.java
@@ -3,6 +3,24 @@ package com.revature.problem;
 public class Solution {
 
     public int mostRecurringInteger(String s) {
-        throw new RuntimeException("TODO! Needs implementation...");
+        int mostRecurring = 0;
+        int temp = 0;
+        int mostRecurringCount = 0;
+        int tempCount = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(temp != Character.getNumericValue(s.charAt(i))){
+                temp = Character.getNumericValue(s.charAt(i));
+                tempCount = 1;
+            }
+            else{
+                tempCount++;
+            }
+            if(tempCount > mostRecurringCount){
+                mostRecurring = temp;
+                mostRecurringCount = tempCount;
+            }
+        }
+        return mostRecurring;
     }
 }
