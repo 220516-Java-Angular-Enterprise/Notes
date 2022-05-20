f532c7ff56399efac1c363934c29fb9174966dd6
Ryan Jacobo <ryanjacobo@yahoo.com>
2022-05-19 21:32:28 -0700

    used solution file

--- coding-challenges/ryanj-mrc/src/main/java/com/revature/problem/Solution.java
+++ coding-challenges/ryanj-mrc/src/main/java/com/revature/problem/Solution.java
@@ -3,6 +3,20 @@ package com.revature.problem;
 import java.util.HashSet;
 
 public class Solution {
+    static char firstMostRecurringInt ( char str[]){
+        HashSet<Character> h = new HashSet<>();
 
+        for (int i = 0; i <= str.length - 1; i++) {
+            char c = str[i];
+
+            if (h.contains(c))
+                return c;
+
+            else
+                h.add(c);
+        }
+
+        return '\0';
+    }
 
 }
--- coding-challenges/ryanj-mrc/src/main/java/com/revature/problem/UI.java
+++ coding-challenges/ryanj-mrc/src/main/java/com/revature/problem/UI.java
@@ -7,21 +7,7 @@ public class UI {
         String str = "11222111";
         str = "1223334444";
         char[] arr = str.toCharArray();
-        System.out.println(firstMostRecurringInt(arr));
+        System.out.println(Solution.firstMostRecurringInt(arr));
     }
-    static char firstMostRecurringInt ( char str[]){
-        HashSet<Character> h = new HashSet<>();
 
-        for (int i = 0; i <= str.length - 1; i++) {
-            char c = str[i];
-
-            if (h.contains(c))
-                return c;
-
-            else
-                h.add(c);
-        }
-
-        return '\0';
-    }
 }
