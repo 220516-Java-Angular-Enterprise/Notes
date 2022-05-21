5289578fb21a8f7ec245e92e2585aca8b6c53849
Pdavis34 <paytondavis40@gmail.com>
2022-05-21 14:15:59 -0400

    updated coding challenge

--- coding-challenges/mrc/src/main/java/com/revature/problem/UI.java
+++ coding-challenges/mrc/src/main/java/com/revature/problem/UI.java
@@ -1,7 +1,19 @@
 package com.revature.problem;
 
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class UI {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter a string: ");
+        String userInput = scan.nextLine();
+
+        System.out.println(sol.mostRecurringInteger(userInput));
 
     }
 }
