fd08c0f28a8528dc64185d526adcc82b2d4c2d1b
Pdavis34 <paytondavis40@gmail.com>
2022-05-21 13:59:06 -0400

    git fixes

--- retangle/rectangleassignment/src/main/java/com/revature/my_triangle/AreaComparator.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.revature.my_triangle;
-
-public class AreaComparator {
-    public boolean isLessThan(Rectangle geethika, Rectangle ryan){
-        return geethika.getArea() < ryan.getArea();
-    }
-}
