ee07df8884e6f25d4920717239688e96677be5d8
Jeff Sturgeon <jeff637@revature.net>
2022-05-22 15:59:53 -0700

    Moved P0

--- week1/wed/assignments/Project_0/src/main/java/com/revature/yolp/ui/IMenu.java
+++
@@ -1,8 +0,0 @@
-package com.revature.yolp.ui;
-
-/* Interface to implement for all menus */
-public interface IMenu {
-
-    /* By default, all interface methods are abstract and final */
-    void start();
-}
