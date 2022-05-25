49bed92fcc72a58a7dcb122f5dfb286081fab3f0
trknox <love.lala2003@gmail.com>
2022-05-25 12:05:28 -0400

    update new branch

---
+++ week1/thu/coding-challenge-solution/src/main/java/com/revature/assignments/UI.java
@@ -0,0 +1,11 @@
+package com.revature.assignments;
+
+public class UI {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println("Solution 1: " + sol.easy(0, 2));
+        System.out.println("Solution 2: " + sol.moderate(3));
+        System.out.println("Solution 3: " + sol.finalBoss("word"));
+    }
+}
